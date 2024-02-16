98d9d1bb3c24c0aabf62944fbc600fa2e0847880
MOHD ABBAS <mohdabbas@deloitte.com>
2024-02-16 17:55:41 +0530

    added

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 data "google_compute_network" "my-network" {
-    project = "${terraform.workspace.project}"
-    name = "${terraform.workspace.name}"
+    project = terraform.workspace
+    name = var.name
 }
